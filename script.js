058cb3e6fc62b85f51751a5998b84d27debf4f0f
Nurbolot <nurchik0225@gmail.com>
2023-05-05 23:33:51 +0600

    Attributes

--- script.js
+++ script.js
@@ -34,24 +34,47 @@ document.addEventListener('keydown', function (e) {
 
 // Создание и вставка элементов
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.innerHTML = 'Мы используем на этом сайте для улучшения функциональности. <button class="btn btn--close-cookie">Ok!</button>';
-const header = document.querySelector('.header');
-header.append(message);
-
-document.querySelector('.btn--close-cookie').addEventListener('click', function() {
-  message.remove();
-});
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML = 'Мы используем на этом сайте для улучшения функциональности. <button class="btn btn--close-cookie">Ok!</button>';
+// const header = document.querySelector('.header');
+// header.append(message);
+//
+// document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+//   message.remove();
+// });
 
 // Стили
 
-message.style.backgroundColor = '#076785';
-message.style.width = '102%';
-console.log(message.style.width);
-console.log(message.style.color);
-console.log(message.style.backgroundColor);
-console.log(getComputedStyle(message));
-console.log(getComputedStyle(message).height);
-message.style.height = Number.parseFloat(getComputedStyle(message).height) + 50 + 'px';
-document.documentElement.style.setProperty('--color-first', 'yellow');
+// message.style.backgroundColor = '#076785';
+// message.style.width = '102%';
+// console.log(message.style.width);
+// console.log(message.style.color);
+// console.log(message.style.backgroundColor);
+// console.log(getComputedStyle(message));
+// console.log(getComputedStyle(message).height);
+// message.style.height = Number.parseFloat(getComputedStyle(message).height) + 50 + 'px';
+// document.documentElement.style.setProperty('--color-first', 'yellow');
+
+// Атрибуты
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+console.log(logo.className);
+
+logo.alt = 'Лого Прекрасного Банка';
+
+// Не стандартный атрибут
+console.log(logo.developer);
+console.log(logo.getAttribute('developer'));
+logo.setAttribute('id', 'logo')
+console.log(logo);
+
+// classes
+
+logo.classList.add('a', 'b');
+logo.classList.remove('a', 'b')
+logo.classList.toggle('a',);
+logo.classList.contains('a');
+
