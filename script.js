fb5eeca0b045a30c4b4b53e2101dc5670c2958b4
Nurbolot <nurchik0225@gmail.com>
2023-05-14 00:48:24 +0600

    Добавлено слайдер с отзывами

--- script.js
+++ script.js
@@ -160,9 +160,25 @@ lazyImages.forEach(image => lazyImagesObserver.observe(image));
 const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
 
 let currentSlide = 0;
 const slidesNumber = slides.length;
+
+const createDots = function() {
+  slides.forEach(function(_, index) {
+    dotContainer.insertAdjacentHTML('beforeend',
+    `<button class='dots__dot' data-slide="${index}"></button>`);
+  })
+}
+createDots();
+
+const activateCurrentDot = function(slide) {
+  document.querySelectorAll('.dots__dot').forEach(dot =>
+  dot.classList.remove('dots__dot--active'))
+  document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active')
+}
+activateCurrentDot(0);
 
 const moveToSlide = function(slide) {
   slides.forEach((s, index) => s.style.transform = `translateX(${(index - slide) * 100}%)`);
@@ -170,8 +186,9 @@ const moveToSlide = function(slide) {
 
   moveToSlide(0);
   // 1 - 0%, 2 - 100%, 3 - 200%, 4 - 300%
+  activateCurrentDot(currentSlide);
 
-btnRight.addEventListener('click', function() {
+const nextSlide=  function() {
   if (currentSlide === slidesNumber - 1) {
     currentSlide = 0;
   } else {
@@ -179,15 +196,33 @@ btnRight.addEventListener('click', function() {
   }
   moveToSlide(currentSlide);
   // 1 - -100%, 2 - -0%, 3 - -100%, 4 - -200%
-})
+  activateCurrentDot(currentSlide);
+}
 
-btnLeft.addEventListener('click', function() {
+const previousSlide = function() {
   if (currentSlide === 0) {
     currentSlide = slidesNumber - 1;
   } else {
     currentSlide--;
   }
   moveToSlide(currentSlide);
+}
+
+btnRight.addEventListener('click', nextSlide);
+
+btnLeft.addEventListener('click', previousSlide);
+
+document.addEventListener('keydown',  function(e) {
+  if (e.key === 'ArrowRight') nextSlide();
+  if (e.key === 'ArrowLeft') previousSlide();
+})
+
+dotContainer.addEventListener('click', function(e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const slide = e.target.dataset.slide;
+    moveToSlide(slide);
+    activateCurrentDot(slide)
+  }
 })
 
 //
